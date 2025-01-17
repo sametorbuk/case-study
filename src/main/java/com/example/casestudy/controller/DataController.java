de5b7b21b154177ae54ee79af2263d295657c0cc
Samet Orbuk <orbuk60@gmail.com>
2025-01-17 13:03:34 +0300

    save endpoint added

--- src/main/java/com/example/casestudy/controller/DataController.java
+++ src/main/java/com/example/casestudy/controller/DataController.java
@@ -2,15 +2,13 @@ package com.example.casestudy.controller;
 
 import com.example.casestudy.entity.Data;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.example.casestudy.service.DataService;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/data")
 @CrossOrigin(origins = "http://localhost:5173")
 public class DataController {
 
@@ -24,13 +22,13 @@ public class DataController {
 
 
     @GetMapping
-    public List<Data> getAllData(){
+    public List<Data> getAllData() {
         return dataService.getAllData();
     }
 
     @PostMapping("/save")
-    public Data save(Data data){
-      return  dataService.save(data);
+    public Data save(Data data) {
+        return dataService.save(data);
     }
 
 }
